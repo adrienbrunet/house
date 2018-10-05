3cb24b2ffc5734fa69b8e9f5df42dd2ead9cb59f
Adrien Brunet <adrien@emencia.com>
2018-10-05 20:26:09 +0200

    chg: renaming app to housing

--- project/settings.py
+++ project/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework.authtoken",
     "rest_framework_swagger",
-    "apps.houses.apps.HousesConfig",
+    "apps.housing.apps.HousingConfig",
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
--- project/tests/tests_installed_apps.py
+++ project/tests/tests_installed_apps.py
@@ -12,6 +12,6 @@ def test_installed_apps():
         "rest_framework",
         "rest_framework.authtoken",
         "rest_framework_swagger",
-        "apps.houses.apps.HousesConfig",
+        "apps.housing.apps.HousingConfig",
     }
     assert apps.issubset(settings.INSTALLED_APPS), settings.INSTALLED_APPS
