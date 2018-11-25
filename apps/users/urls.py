4e936cf701a87fced8f3aaab954acf4c4cc64bee
Adrien Brunet <adrien@emencia.com>
2018-11-25 21:06:12 +0100

    chg: Update users/urls, adding trailing slashes

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -5,13 +5,13 @@ from . import views
 
 app_name = "users"
 urlpatterns = [
-    path("me", views.UserDetailView.as_view(), name="me"),
+    path("me/", views.UserDetailView.as_view(), name="me"),
     path(
-        "me/password",
+        "me/password/",
         views.UserChangePasswordView.as_view(),
         name="user-change-password",
     ),
-    path("auth/signup", views.SignupView.as_view(), name="signup"),
-    path("auth/login", views.ObtainAuthTokenView.as_view(), name="login"),
-    path("auth/reset", views.ResetPasswordView.as_view(), name="reset-password"),
+    path("auth/signup/", views.SignupView.as_view(), name="signup"),
+    path("auth/login/", views.ObtainAuthTokenView.as_view(), name="login"),
+    path("auth/reset/", views.ResetPasswordView.as_view(), name="reset-password"),
 ]
