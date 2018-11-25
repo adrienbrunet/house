0f92e8ce1aa4973f7798f90d3fae9e16e121dec2
Adrien Brunet <adrien@emencia.com>
2018-11-25 21:06:45 +0100

    chg: :white_check_mark: Adding new apps in tests_installed_apps

--- project/tests/tests_installed_apps.py
+++ project/tests/tests_installed_apps.py
@@ -16,5 +16,7 @@ def test_installed_apps():
         "apps.users.apps.UsersConfig",
         "apps.addresses.apps.AddressesConfig",
         "apps.contact.apps.ContactConfig",
+        "apps.groups.apps.GroupsConfig",
+        "apps.bookings.apps.BookingsConfig",
     }
     assert apps.issubset(settings.INSTALLED_APPS), settings.INSTALLED_APPS
